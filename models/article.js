00ed8a1b96df968f2cb22c0b45e5b8598d7b9549
Sean Esteva <seangeleno@gmail.com>
2016-01-28 04:54:43 -0800

    added article.js model and module.exports for Articles and Id

---
+++ models/article.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+
+var articleShema = mongoose.Schema({
+  title: {
+    type: String,
+    index: true,
+    required: true
+  },
+  body {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    index: true,
+    required: true
+  },
+  date: {
+    type: date,
+    default: Date.now
+  }
+});
+
+var article = module.exports = mongoose.model('Artile', articleSchema);
+
+//Get all articles
+module.exports.getArticles = function (callback){
+  Article.find(callback);
+}
+
+// Get article by ID
+module.exports.getArticleById = function(id, callback){
+  Article.findById(id, callback);
+}
+
+//Get Category articles
+//We want article category field to match the category thats passed in
+module.exports.getArticleByCategory = function(category, callback){
+  var query = {category: category };
+  Article.find(query, callback)
+}
+
+//
