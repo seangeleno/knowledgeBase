d7a4ad140c01babbeae2a72e8920463417be84ae
Sean Esteva <seangeleno@gmail.com>
2016-01-28 11:48:10 -0800

    fixed controller naming typo

--- public/controllers/articles.controller.js
+++ public/controllers/articles.controller.js
@@ -5,3 +5,10 @@ angular.module("kb")
 		$scope.articles = data;
 	});
 }])
+
+.controller('ArticlesCategoriesCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams){
+	$http.get('/articles/category/'+$routeParams.category).success(function(data){
+		$scope.cat_articles = data;
+		$scope.category = $routeParams.category;
+	});
+}])
