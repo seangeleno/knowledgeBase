2301794f729683518ae35b98059e021dc353e772
Sean Esteva <seangeleno@gmail.com>
2016-01-28 06:20:51 -0800

    added module.exports to create, edit and delete an article

--- models/article.js
+++ models/article.js
@@ -41,4 +41,35 @@ module.exports.getArticleByCategory = function(category, callback){
   Article.find(query, callback)
 }
 
-//
+// Add an Article
+
+module.exports.createArticle = function(newArticle, callback){
+  newArticle.save(callback);
+}
+
+//Update an article
+module.exports.updateArticle = function(id, data, callback){
+  var title    = data.title;
+  var body     = data.body;
+  var category = data.category;
+
+  var query = {_id: id};
+
+  Article.findbyId(id, function(err, article){
+    if(!article){
+      return next(new Error('Could not load article'));
+    } else {
+      //Update
+      article.title    = title;
+      article.body     = body;
+      article.category = category;
+
+      article.save(callback);
+    }
+  });
+}
+
+// Remove Article
+module.exports.removeArticle = function(id, callback){
+  Article.find({_id:id}).remove(callback);
+}
