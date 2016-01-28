d98515f20dae3eb6778ed62867b881f4c968f549
Sean Esteva <seangeleno@gmail.com>
2016-01-28 05:05:05 -0800

    added Article variable that references model schema

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var Article = require('../models/article') //this file is giving us accecss to the mongoose article model object
+
 /* GET users listing. */
+//Since we're in the file articles, we don't need to specify /articles, it would come out as /articles/articles
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Article.getArticles(function(err, articles){
+    if(err){
+      console.log(err);
+    }
+    res.json(articles);
+  })
 });
 
 module.exports = router;
