52f570036fac9872a4a3128132c3ba73c9ea217b
Sean Esteva <seangeleno@gmail.com>
2016-01-28 06:10:13 -0800

    added category routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var Categorie = require('../models/category') //this file is giving us accecss to the mongoose category model object
+
 /* GET users listing. */
+//Since we're in the file categories, we don't need to specify /categories, it would come out as /categories/categories
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Categorie.getCategories(function(err, categories){
+    if(err){
+      console.log(err);
+    }
+    res.json(categories);
+  })
+});
+
+//adding route for individual id
+//req.params.id has to match '/:id' you can put whatever you want as long as they match
+router.get('/:id', function(req, res, next) {
+  Categorie.getCategoryById(req.params.id, function(err, category){
+    if(err){
+      console.log(err);
+    }
+    res.json(category);
+  })
 });
 
 module.exports = router;
