2df51d1ae7c1232b0efb164996d3ddddbfd1890d
Sean Esteva <seangeleno@gmail.com>
2016-01-28 08:14:54 -0800

    added basic views and routing to edit delete articles

--- public/app.js
+++ public/app.js
@@ -6,4 +6,25 @@ app.config(['$routeProvider', function($routeProvider){
       templateUrl: 'views/categories.view.html',
       controller: 'CategoriesCtrl'
     })
+    .when('/articles', {
+      templateUrl: 'views/articles.view.html',
+      controller: 'ArticlesCtrl'
+    })
+    .when('/articles/details/:id', {
+      templateUrl: 'views/article_details.view.html',
+      controller: 'ArticlesDetailsCtrl'
+    })
+    .when('/articles/category/:category', {
+      templateUrl: 'views/cat_articles.view.html',
+      controller: 'ArticlesCategoriesCtrl'
+    })
+    .when('/articles/add', {
+      templateUrl: 'views/add_article.view.html',
+      controller: 'ArticlesCreateCtrl'
+    })
+    .when('/articles/edit/:id', {
+      templateUrl: 'views/edit_article.view.html',
+      controller: 'ArticlesEditCtrl'
+    })
+    .otherwise({redirectTo: '/categories'})
 }]);
